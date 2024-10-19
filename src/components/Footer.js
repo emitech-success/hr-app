8d1a87248a1021e62a9fce9d1d259e6248ba3b40
emitech-success <odezuluaugustine@gmail.com>
2024-10-19 10:42:57 +0100

    added link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ const Footer = () => {
     <footer className="footer">
       <p>&copy; 2024 HR Solutions. All Rights Reserved.</p>
       <div className="social-links">
-        <a href="#">LinkedIn</a>
-        <a href="#">Twitter</a>
-        <a href="#">Facebook</a>
+        <a href="/">LinkedIn</a>
+        <a href="/">Twitter</a>
+        <a href="/">Facebook</a>
       </div>
     </footer>
   );
